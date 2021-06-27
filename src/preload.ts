e20c8b249f39f9751ce2fbcdaca12f5688ea603d
Free 公園 <45021001+FreeAoi@users.noreply.github.com>
2021-06-27 07:05:30 +0200

    tab bug fix xd

--- src/preload.ts
+++ src/preload.ts
@@ -4,6 +4,6 @@ contextBridge.exposeInMainWorld('MP3DownloaderAPI', {
     sendToWindow: (type: string) => ipcRenderer.send('window-buttons', type ?? ''),
     download: (id: string, title: string) => ipcRenderer.send('start-download', { id, title }),
     onProgress: (cb: any) => ipcRenderer.on('download-progress', cb),
-		downloadPath: (path: string) => ipcRenderer.send("downloadPath", { path }),
+    downloadPath: (path: string) => ipcRenderer.send("downloadPath", { path }),
     selectDir: () => ipcRenderer.send("select-directory")
 });
